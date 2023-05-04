175ab29c9ddd46d423bdd24afa748a4992a2eb23
sergiolara <sergiolaradiaz3@gmail.com>
2023-05-04 16:14:14 -0500

    termino-maquetacion

---
+++ js/app.js
@@ -0,0 +1,4 @@
+const carrito = document.querySelector("#carrito");
+const contenedorCarrito = document.querySelector("#lista-carrito tbody");
+const vaciarCarritoBtn = document.querySelector("#vaciar-carrito");
+const listaCursos = document.querySelector("#lista-cursos");
